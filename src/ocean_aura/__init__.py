afb9bb710703c12fbab9d9173fb3bfc6f4ba6d31
yujiaxueredaiyu <320662591@qq.com>
2026-07-09 11:59:56 +0800

    re4

--- src/ocean_aura/__init__.py
+++ src/ocean_aura/__init__.py
@@ -1,10 +1,5 @@
 from .audio import AudioSystem
-from .particles import Particle, StarParticle
-from .fish import Fish
-from .hand_detection import HandDetector
-from .visualization import OceanVisualizer
 from .flow_field import FlowField
-from .main import OceanAura, main
 from .config import (
     DEFAULT_WIDTH,
     DEFAULT_HEIGHT,
@@ -21,14 +16,7 @@ from .config import (
 
 __all__ = [
     'AudioSystem',
-    'Particle',
-    'StarParticle',
-    'Fish',
-    'HandDetector',
-    'OceanVisualizer',
     'FlowField',
-    'OceanAura',
-    'main',
     'DEFAULT_WIDTH',
     'DEFAULT_HEIGHT',
     'PARTICLE_CONFIG',
